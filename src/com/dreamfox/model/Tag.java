ba4284a23ff6834a2399057988d37fe431660ff4
JonmuxuPvP <Jonmuxupvp@gmail.com>
2021-09-02 17:08:14 +0200

    Added Tag class

---
+++ src/com/dreamfox/model/Tag.java
@@ -0,0 +1,37 @@
+package com.dreamfox.model;
+
+public class Tag {
+    private String name;
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Tag)) {
+            return false;
+        } else {
+            Tag anotherTag = (Tag) obj;
+            return this.name.equals(anotherTag.name);
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
